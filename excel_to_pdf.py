0def9b5a1e5b713e94a9faebac3cc3290a9a827c
Hoa Tran <hoa.tran@gigacover.com>
2019-09-16 22:39:55 +0700

    Demo file python and mock data

---
+++ excel_to_pdf.py
@@ -0,0 +1,31 @@
+import sys
+import subprocess
+import os
+
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+template_path = f'{PROJECT_ROOT}/MOCK_DATA.xlsx'
+path = f'{PROJECT_ROOT}/'
+
+
+def convert_to(timeout=None):
+    args = [libreoffice_exec(), '--headless', '--convert-to', 'pdf', '--outdir', path, template_path]
+
+    process = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=timeout)
+    filename = os.path.isfile(f'{path}MOCK_DATA.pdf')
+    if not filename:
+        raise Exception("Can not convert")
+    else:
+        return 'MOCK_DATA.pdf'
+
+
+def libreoffice_exec():
+    if sys.platform == 'darwin':
+        return '/Applications/LibreOffice.app/Contents/MacOS/soffice'
+    return 'libreoffice'
+
+
+
+if __name__ == '__main__':
+    print('Converted to ' + convert_to())
